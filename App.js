e27f925a349b73f3b4fc45027a601276106492ca
fajarrrX <fajarrr30@gmail.com>
2021-03-13 11:08:23 +0700

    add navigation feature: 1. stack screen between splash and landing page. 2. create tab screen navigation between home and detail pokemon

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Home, Detail, SplashScreen } from "./src/screens";
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function LandingPage() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Detail" component={Detail} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Stack.Navigator>
+        <Stack.Screen name="LandingPage" component={LandingPage} options={{ headerShown: false }}/>
+        <Stack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/screens/SplashScreen/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+export default function SplashScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Splash Screen</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
